0f68db8449713d5a7c0cc1c1e98786182d949c0c
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-10-11 18:02:33 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -22,3 +22,20 @@ module Findable
   end
 end
 
+module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+end
